745ad76665f7a67181245d1cc00641ecc2e25675
alxmart <alxmartinez@gmail.com>
2023-07-23 19:48:25 -0300

    Created new activity ResultadoActivity

--- app/src/main/java/com/luizafmartinez/calculadorimc/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/calculadorimc/MainActivity.kt
@@ -2,10 +2,21 @@ package com.luizafmartinez.calculadorimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnCalcular: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCalcular = findViewById(R.id.btn_calcular)
+        btnCalcular.setOnClickListener {
+
+        }
+
+
+
     }
 }
